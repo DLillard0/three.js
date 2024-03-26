7205560ce63ee37907b6fec6228d981ea0233805
DLillard0 <565570086@qq.com>
2024-03-26 22:25:15 +0800

    learn

--- learn/index.js
+++ learn/index.js
@@ -10,34 +10,35 @@ function main() {
   const fov = 75
   const aspect = 1
   const near = 0.1
-  const far = 5
+  const far = 100
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-  camera.position.z = 2
+  camera.position.set(4, 2, 4)
+  camera.lookAt(0, 0, 0)
 
   const scene = new THREE.Scene()
 
-  const ambientLight = new THREE.AmbientLight(0xFFFFFF, 2)
+  const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1)
   scene.add(ambientLight)
 
-  {
-    const color = 0xFFFFFF
-    const intensity = 3
-    const light = new THREE.DirectionalLight(color, intensity)
-    light.position.set(-1, 2, 4)
-    scene.add(light)
-  }
+  // {
+  //   const color = 0xFFFFFF
+  //   const intensity = 3
+  //   const light = new THREE.DirectionalLight(color, intensity)
+  //   light.position.set(-1, 2, 4)
+  //   scene.add(light)
+  // }
 
-  {
-    const color = 0xFFFF00
-    const intensity = 3
-    const light = new THREE.PointLight(color, intensity)
-    light.position.set(1, 2, 4)
-    scene.add(light)
-  }
+  // {
+  //   const color = 0xFFFF00
+  //   const intensity = 3
+  //   const light = new THREE.PointLight(color, intensity)
+  //   light.position.set(1, 2, 4)
+  //   scene.add(light)
+  // }
 
-  const boxWidth = 1
-  const boxHeight = 1
-  const boxDepth = 1
+  const boxWidth = 2
+  const boxHeight = 2
+  const boxDepth = 2
   const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth)
 
   function makeInstance(geometry, color, x) {
@@ -49,7 +50,7 @@ function main() {
   }
 
   const cubes = [
-    makeInstance(geometry, 0x44aa88, 0),
+    makeInstance(geometry, 0xff0000, 0),
     // makeInstance(geometry, 0x8844aa, -2),
     // makeInstance(geometry, 0xaa8844, 2),
   ]
@@ -61,11 +62,11 @@ function main() {
       camera.updateProjectionMatrix()
     }
 
-    cubes.forEach((cube, index) => {
-      const rot = time * 0.001 * (1 + index * 0.1)
-      cube.rotation.x = rot
-      cube.rotation.y = rot
-    })
+    // cubes.forEach((cube, index) => {
+    //   const rot = time * 0.001 * (1 + index * 0.1)
+    //   cube.rotation.x = rot
+    //   cube.rotation.y = rot
+    // })
     renderer.render(scene, camera)
     // requestAnimationFrame(render)
   }
